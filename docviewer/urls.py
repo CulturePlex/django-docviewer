17f5dbfcad3a0f41d4763075f61d9e78d0f4f512
mavillard <ajimene6@uwo.ca>
2014-10-14 13:16:45 -0400

    Redirecting to loggin

--- docviewer/urls.py
+++ docviewer/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls.defaults import patterns
 from docviewer.views import JsonDocumentView, SearchDocumentView
 from django.views.generic import DetailView
 from docviewer.models import Document
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = patterns(
     '',
     (r'^doc-(?P<pk>\d+)\.json$', JsonDocumentView.as_view(), {}, "docviewer_json_view"),
-    (r'^(?P<pk>\d+)/(?P<slug>.+)\.html$', DetailView.as_view(context_object_name='document', model=Document), {}, "docviewer_viewer_view"),
+    (r'^(?P<pk>\d+)/(?P<slug>.+)\.html$', login_required(DetailView.as_view(context_object_name='document', model=Document)), {}, "docviewer_viewer_view"),
     (r'^search/(?P<pk>\d+)/(?P<slug>.+)\.json$', SearchDocumentView.as_view(), {}, "docviewer_search_view"),
     (r'^print-annotations/(?P<pk>\d+)/(?P<slug>.+)\.html$', JsonDocumentView.as_view(), {}, "docviewer_printannotations_view"),
     (r'^(?P<pk>\d+)/update_annotation/$', 'docviewer.views.update_annotation', {}, "docviewer_annotation"),
